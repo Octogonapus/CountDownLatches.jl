b14e0cab3b92d26ad654ca6f45581f02b0cf2e20
Octogonapus <firey45@gmail.com>
2021-03-16 22:25:00 -0400

    Initial implementation

--- src/CountDownLatch.jl
+++ src/CountDownLatch.jl
@@ -1,5 +1,41 @@
 module CountDownLatch
 
-# Write your package code here.
+export Latch, await, count_down, get_count
+
+mutable struct Latch
+    count::Threads.Atomic{Int64}
+    condition::Threads.Condition
+
+    function Latch(count::I) where {I<:Integer}
+        if count < 0
+            throw(ArgumentError("Count ($count) must not be negative."))
+        end
+
+        new(Threads.Atomic{Int64}(count), Threads.Condition(ReentrantLock()))
+    end
+end
+
+function await(latch::Latch)
+    lock(latch.condition) do
+        while true
+            if get_count(latch) == 0
+                return
+            end
+            wait(latch.condition)
+        end
+    end
+end
+
+function count_down(latch::Latch)
+    Threads.atomic_sub!(latch.count, 1)
+    lock(latch.condition) do
+        notify(latch.condition, nothing; all = true, error = false)
+    end
+    nothing
+end
+
+function get_count(latch::Latch)
+    latch.count[]
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,77 @@
 using CountDownLatch
 using Test
+using ThreadPools
 
-@testset "CountDownLatch.jl" begin
-    # Write your tests here.
+@testset "create a latch with a negative count" begin
+    @test_throws ArgumentError Latch(-1)
+end
+
+@testset "count down from 1" begin
+    latch = Latch(1)
+    count_down(latch)
+    await(latch)
+    @test get_count(latch) == 0
+end
+
+@testset "wait for another thread to count down from 1" begin
+    latch = Latch(1)
+
+    t = Threads.@spawn begin
+        count_down(latch)
+    end
+    
+    await(latch)
+    
+    @test get_count(latch) == 0
+    @test istaskdone(t)
+end
+
+@testset "mutual count down" begin
+    latch1 = Latch(1)
+    latch2 = Latch(1)
+    latch3 = Latch(1)
+
+    t = Threads.@spawn begin
+        count_down(latch2)
+        await(latch1)
+        count_down(latch3)
+    end
+
+    await(latch2)
+    count_down(latch1)
+    await(latch3)
+
+    @test get_count(latch1) == 0
+    @test get_count(latch2) == 0
+    @test get_count(latch3) == 0
+    @test istaskdone(t)
+end
+
+@testset "wait for another thread to count down from 2" begin
+    latch = Latch(2)
+    
+    t = Threads.@spawn begin
+        count_down(latch)
+        count_down(latch)
+    end
+    
+    await(latch)
+    
+    @test get_count(latch) == 0
+    @test istaskdone(t)
+end
+
+@testset "wait for this thread to count down from 2 using ThreadPools" begin
+    latch = Latch(2)
+
+    this_thread_id = Threads.threadid()
+    @tspawnat this_thread_id begin
+        # I would like to assert that await() was called but this is the best I can come up with
+        @test get_count(latch) == 1
+        count_down(latch)
+    end
+    count_down(latch)
+    await(latch)
+    
+    @test get_count(latch) == 0
 end
