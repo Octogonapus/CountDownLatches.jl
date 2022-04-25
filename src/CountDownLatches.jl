2a8bc1f28e39b027e13a88d943a84f1021eeb5a2
Octogonapus <firey45@gmail.com>
2022-04-25 10:02:47 -0400

    Fix atomic_sub on 32-bit ubuntu 1.7 (#3)
    * Fix atomic_sub on 32-bit ubuntu 1.7
    
    * Fix the same issue in the constructor
    
    * Fix the same issue in the st ruct
    
    * Reduce CI matrix
    
    * Add VersionVigilante
    
    * Fix VV main branch

--- src/CountDownLatches.jl
+++ src/CountDownLatches.jl
@@ -7,8 +7,8 @@ export CountDownLatch, await, count_down, get_count
 
 A `CountDownLatch` that starts at some initial non-negative `count`. This latch can be counted down and waited on.
 """
-mutable struct CountDownLatch
-    count::Threads.Atomic{Int64}
+mutable struct CountDownLatch{I<:Integer}
+    count::Threads.Atomic{I}
     condition::Threads.Condition
 
     function CountDownLatch(count::I) where {I<:Integer}
@@ -16,7 +16,7 @@ mutable struct CountDownLatch
             throw(ArgumentError("Count ($count) must not be negative."))
         end
 
-        new(Threads.Atomic{Int64}(count), Threads.Condition(ReentrantLock()))
+        new{I}(Threads.Atomic{I}(I(count)), Threads.Condition(ReentrantLock()))
     end
 end
 
@@ -44,22 +44,8 @@ end
 Counts down the `latch` once. This may cause the count to become negative. Any tasks that were blocked in `await` will
 be woken if the count becomes `<= 0`.
 """
-function count_down(latch::CountDownLatch)
-    # I think doing something like:
-    #
-    # 1: count = get_count(latch)
-    # 2: count == 0 && return
-    # 3: next_count = count - 1
-    # 4: if Threads.atomic_cas!(latch.count, count, next_count) == count
-    # 5:     lock(latch.condition) do
-    # 6:         notify(latch.condition, nothing; all = true, error = false)
-    # 7:     end
-    # 8: end
-    #
-    # is wrong here because of the case where a thread decrements count after line 1 and before line 4. In that case,
-    # two calls to `notify` must still happen, but only one would.
-
-    Threads.atomic_sub!(latch.count, 1)
+function count_down(latch::CountDownLatch{I}) where {I<:Integer}
+    Threads.atomic_sub!(latch.count, I(1))
     lock(latch.condition) do
         notify(latch.condition, nothing; all = true, error = false)
     end
